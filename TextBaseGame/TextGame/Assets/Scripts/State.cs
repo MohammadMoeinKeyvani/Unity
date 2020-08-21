3c4e305aacb53df82478ab94aa68bb744b99d840
MohammadMoeinKeyvani <moeink1e@gmail.com>
2020-08-22 03:13:40 +0430

    Text Base Game

---
+++ TextBaseGame/TextGame/Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class State : ScriptableObject
+{
+     [TextArea(15,10)] [SerializeField] string storyText;
+     [SerializeField] State[] nextState;
+
+     public string getStateStory()
+     {
+          return storyText;
+     }
+
+     public State[] getNextStates()
+     {
+          return nextState;
+     }
+}
